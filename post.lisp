6efd2b7d5e910a7293dca41d87410bf6785db119
Alice Wyan <alice@finitud.org>
2013-01-02 19:20:48 +0000

    Initial work on post class

---
+++ post.lisp
@@ -0,0 +1,25 @@
+(in-package #:notes)
+
+(defclass post ()
+  ((text :accessor post-text
+	 :initform ""
+	 :initarg :text)
+   (id :accessor post-id
+       :initarg :id)
+   (timestamp :accessor post-timestamp
+	      :initform (get-universal-time)
+	      :initarg :timestamp)))
+
+(defgeneric print-post (post))
+
+(defmethod print-post ((post post))
+  (yaclml:with-yaclml-output-to-string
+    (<:div :class "post"
+	   (<:h3 (<:format "#~a" (post-id post)))
+	   (<:h5 (<:format "[~a]" (post-timestamp post)))
+	   (<:p (<:format "~a" (post-text post))))))
+
+(defun add-post (text)
+  (vector-push-extend (make-instance 'post :text text
+				     :id (fill-pointer *posts*))
+  )
